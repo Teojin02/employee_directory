2cbbb5d675fe6e685e2df5f4cf495cfd60758af6
Your Name <you@example.com>
2020-10-17 12:38:42 -0700

    deleted folders

--- directory/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Home() {
-    return (
-        <h1>Testing right here</h1>
-    )
-}
-
-export default Home;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
-import Home from "./components/Home"
+import React from 'react';
+import './App.css';
+import { Component } from 'react';
+import EmployeeList from './EmployeeList';
+import SearchBox from './SearchBox';
 
-function App() {
-    return <Home />;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      employees: [
+        {name:"Dwight", position:"Manager"},
+        {name:"Pam" , position:"Sales"},
+        {name:"Jim" , position:"Sales"},
+        {name:"Phyilis" , position:"Sales"},
+        {name:"Stanley" , position:"Sales"}
+      ],
+      searchEmployee:''
+    }
+  }
+
+  handleInput = (e) => {
+    console.log(e.target.value);
+    this.setState({searchEmployee: e.target.value })
+  }
+  render () {
+    let filteredEmployees = this.state.employees.filter((Employee) =>{
+      return Employee.name.toLowerCase().includes(this.state.searchEmployee.toLowerCase())
+    })
+
+    return (
+      <div className="App">
+        <h1>Dunder Mifflin Employee's </h1>
+        <SearchBox handleInput={this.handleInput}/>,
+        <EmployeeList filteredEmployees={filteredEmployees}  />
+      </div>
+    );
+  }
 }
-
-
-
 export default App;
